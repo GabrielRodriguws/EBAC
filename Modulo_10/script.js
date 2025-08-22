bfd8a8ed390a521a84219125717f26bf2be9469d
Gabriel Rodrigues da Silva <gabriel.rodrigues.727@outlook.com>
2025-08-22 16:58:47 -0300

    add imagens extra a pasta imagens. add mascara para o formulario além de validação para o mesmo

--- Modulo_10/script.js
+++ Modulo_10/script.js
@@ -6,4 +6,30 @@ $(document).ready(function () {
     $('.hamburgerMenu').click(function(){
         $('nav').slideToggle();
     })
+
+    $('#telefone').mask('(00) 00000-0000',{
+        placeholder: '(00) 00000-0000'
+    });
+
+    $('.listaVeiculos button').click(function () { 
+        const destino = $('#contato');
+        $('html').animate({
+            scrollTop: destino.offset().top
+        },1000)
+    });
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            }
+        }
+    })
 });
